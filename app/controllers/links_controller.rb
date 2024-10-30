46aaf5a998019ae0c273627b7e50c78485fa045e
Adam <adam.krukowski2021@gmail.com>
2024-10-30 22:57:06 +0000

    setting up links controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,30 @@
+class LinksController < ApplicationController
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to short_path(slug: @link.slug)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @link = Link.find_by(slug: params[:slug])
+    if @link
+      @link.increment!(:clicked)
+      redirect_to @link.url
+    else
+      render 'errors/404', status: :not_found
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :slug)
+  end
+end
